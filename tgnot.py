5e8ed76261e1f4a6f3347b27e109ca535227a087
Walker835 <47429110+mstkyvz@users.noreply.github.com>
2023-01-27 11:07:29 +0300

    Add files via upload

---
+++ tgnot.py
@@ -0,0 +1,31 @@
+import requests
+import time
+import urllib
+# Telegram Bot API token
+
+TOKEN = 'TOKEN'
+
+# Telegram kullanıcı ID
+CHAT_ID = 'CHAT_ID'
+
+# Kontrol etmek istediğiniz web sayfasının URL'si
+URL = 'https://www.example.com/'
+
+# İlk çekim için içeriği kaydedin
+first_content = requests.get(URL).text
+
+while True:
+    current_content = requests.get(URL).text
+    soup = BeautifulSoup(current_content, 'html.parser')
+    navlist = soup.select('.navlist')
+    if current_content != first_content or (navlist and navlist != first_navlist):
+        params = {
+            'chat_id': CHAT_ID,
+            'text': "Web sayfasında değişiklikler tespit edildi! " + URL
+        }
+        bot_url = f'https://api.telegram.org/bot%7BTOKEN%7D/sendMessage?'
+        params_encoded = urllib.parse.urlencode(params)
+        requests.get(bot_url + params_encoded)
+        first_content = current_content
+        first_navlist = navlist
+    time.sleep(15)
